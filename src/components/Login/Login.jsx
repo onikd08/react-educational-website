9c66a6a157a7a8785e3cfd2ecde92885d49e41c3
Anik Das <onikd08@gmail.com>
2023-04-27 09:47:48 +0300

    Add login and sign up components

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,55 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <h3>Login</h3>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                required
+                name="email"
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                required
+                name="password"
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+              />
+              <label className="label">
+                <Link to="/signup" className="label-text-alt link link-hover">
+                  Don't have an account yet?
+                </Link>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,9 +1,67 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
-    <div>
-      <h3>Signup</h3>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold">Please Sign Up!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Full Name</span>
+              </label>
+              <input
+                required
+                name="name"
+                type="text"
+                placeholder="name"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                required
+                name="email"
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                required
+                name="password"
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+              />
+              <label className="label">
+                <Link to="/login" className="label-text-alt link link-hover">
+                  Already have an account?
+                </Link>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
