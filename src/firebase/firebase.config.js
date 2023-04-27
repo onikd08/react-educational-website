adb73196ca1a6aea2831d38deb513913a2428492
Anik Das <onikd08@gmail.com>
2023-04-27 09:50:29 +0300

    Add firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDHG77RmN5dkXTqOMadTbqzBFHcT1bzsQY",
+  authDomain: "react-education-codepoint.firebaseapp.com",
+  projectId: "react-education-codepoint",
+  storageBucket: "react-education-codepoint.appspot.com",
+  messagingSenderId: "730790517043",
+  appId: "1:730790517043:web:cf4f95bea3e88ad6670646",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
